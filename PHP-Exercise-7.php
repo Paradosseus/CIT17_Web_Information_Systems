108590ac39792cd303a6d6d7b7bad34dd4e44171
CIT173F-46@M307.UC-BCF.EDU.PH <CIT173F-46@M307.UC-BCF.EDU.PH>
2022-09-22 10:40:35 +0800

    revisions on exercise 7

--- PHP-Exercise-7.php
+++ PHP-Exercise-7.php
@@ -1,26 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>PHP Exercise 7</h1>
-
-    <?php
-        echo "<table border=\"1\">";
-
+<?php
         for ($row = 1; $row <= 7; $row++) {
-            echo "<tr>\n";
+            echo "\n";
             for ($col = 1; $col <= 7; $col++) {
                 $multi = $row * $col;   
-                echo "<td>$multi</td>\n";
+                echo "$multi" . "\t";
             }
-            echo "</tr>";
+            
         }
-        echo "</table>"
-    ?>
-</body>
-</html>
+        
+?>
