45eb1682501d2bcf7bd652a8ce200bf8fd47ea76
CIT173F-46@M307.UC-BCF.EDU.PH <CIT173F-46@M307.UC-BCF.EDU.PH>
2022-09-22 10:20:16 +0800

    revisions

--- PHP-Exercise-5.php
+++ PHP-Exercise-5.php
@@ -1,18 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>PHP Exercise 5.1</h1>
-
-    <?php
-        $around = "around";
+<?php
+	$around = "around";
 
         echo 'What goes ' . $around . ' , comes ' . $around . '.'; 
-    ?>
-</body>
-</html>
+?>
--- PHP-Exercise-6.php
+++ PHP-Exercise-6.php
@@ -1,20 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>PHP Exercise 6</h1>
-
-    <?php
-        for($i = 1; $i <= 12; $i++) {
+<?php
+	for($i = 1; $i <= 12; $i++) {
             $square =  $i * $i;
             echo $i . ' * ' . $i . ' = ' . $square;
-            echo '<br>';
+            echo "\n";
         }
-    ?>
-</body>
-</html>
+?>
